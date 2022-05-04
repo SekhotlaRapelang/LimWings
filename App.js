a3d1e8dba3987bf40b735fd49e46139c1c6ef888
sekhotlarapelang <sekhotlarapelang@gmail.com>
2022-05-04 20:32:42 +0200

    pictures insertion and code fixing

--- App.js
+++ App.js
@@ -31,19 +31,21 @@ export default function App() {
         <Feather name="more-vertical" size={30} color="white"  style={{marginStart:48, backgroundColor:"rgb(189, 189, 189)",borderRadius:8}}/>
         </TouchableOpacity>
         </View>
-        <Text style={{fontSize: 24, marginTop:35 , color:'rgb(255, 208, 42)'}}>The total Price is {totalPrice}</Text>
+        <Text style={{fontSize: 24, marginTop:35 , color:'rgb(255, 208, 42)'}}>The total Price:M{totalPrice}.00</Text>
         </View>
 
-      < ScrollView>
-      <Contact name="Bread,eggs & tea or juice" price ={15} totalPrice={totalPrice} setTotalPrice={setTotalPrice} Image source={require('./assets/Egg.jpg')} />
-      <Contact name="Bread,eggs,vegetables,cheese & juice"price ={18} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="Bread, eggs, bacon, vegetables & juice" price ={25} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="Rice, vegetables and pork " price ={18} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="Rice, vegetables and beef " price ={25} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="Rice, vegetables and chicken " price ={28} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="papa, vegetables beef" price ={30} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="papa, vegetables pork"  price ={38} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
-      <Contact name="papa, vegetables chicken" price ={40} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
+      <ScrollView>
+      < Text style={styles.text}>BREAKFASt</Text>
+      <Contact name="Bread,eggs & tea or juice" price ={15} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/Egg.jpg")} profile={require("./assets/Egg.jpg")}/>
+      <Contact name="Bread,eggs,vegetables,cheese & juice"price ={18} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/vc.jpeg")}  profile={require("./assets/images.jpg")}/>
+      <Contact name="Bread, eggs, bacon, vegetables & juice" price ={25} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/beacon.jpg")} profile={require("./assets/image.jpg")}/>
+      < Text style={styles.text}>LUNCH</Text>
+      <Contact name="Rice, vegetables and pork " price M ={18} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/Rice.jpg")}  profile={require("./assets/rc.webp")}/>
+      <Contact name="Rice, vegetables and beef " price ={25} totalPrice={totalPrice} setTotalPrice={setTotalPrice}  background={require("./assets/pork.jpg")}  profile={require("./assets/rbf.jpg")}/>
+      <Contact name="Rice, vegetables and chicken " price ={28} totalPrice={totalPrice} setTotalPrice={setTotalPrice}  background={require("./assets/chicken.jpg")}  profile={require("./assets/chick.jpg")}/>
+      <Contact name="Papa, vegetables beef" price ={30} totalPrice={totalPrice} setTotalPrice={setTotalPrice}  background={require("./assets/food.jpg")}  profile={require("./assets/beef.webp")}/>
+      <Contact name="Papa, vegetables pork"  price ={38} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/papa.jpg")}  profile={require("./assets/prk.jpg")}/>
+      <Contact name="Chips, vegetables chicken" price ={40} totalPrice={totalPrice} setTotalPrice={setTotalPrice} background={require("./assets/chips.jpg")}  profile={require("./assets/chp.jpg")}/>
  
       </ScrollView>
       
@@ -56,8 +58,8 @@ export default function App() {
          <TouchableOpacity>
          <Text style={{marginTop:6, marginLeft:10,color: 'white', fontSize:19}}>Search Here</Text>
          </TouchableOpacity>
-       </View>
-       
+         </View>
+         
     </View>
   );
 }
@@ -70,9 +72,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   seContainer: {
+
     height: '90%',
     width: '100%',
-    backgroundColor: 'white',
+    backgroundColor: 'rgb(245, 245, 245)',
   },
   Con:{
     backgroundColor:'rgb(33, 33, 33)',
@@ -87,6 +90,12 @@ const styles = StyleSheet.create({
     height:'22.58%',
     width:360,
     marginLeft:0,
-    
   },
+   text:{
+     fontSize: 20,
+     fontWeight: 'bold',
+     backgroundColor: 'black',
+     marginTop:10,
+     color:'white'
+   }
 });
--- Customer.js
+++ Customer.js
@@ -1,30 +1,33 @@
 import { StyleSheet, Text, View, Image, Button, ImageBackground } from 'react-native';
 import {useState} from 'react'
 
-export default function Contact({name, totalPrice, setTotalPrice,price }) {
-  
+export default function Contact({name, totalPrice, setTotalPrice,price, background, profile }) {
   const [iterms, setIterms] = useState(0);
+  const initialPrice = price;
+
   function AddIterms(){
     setIterms(iterms + 1)
-    price = price + 15;
-    setTotalPrice(totalPrice + 15)
+    price += initialPrice;
+    setTotalPrice(totalPrice + initialPrice)
     }
     function CancelIterms(){
       setIterms(iterms - 1)
-      price = price - 15;
-      setTotalPrice(totalPrice - 15)
+      price -= initialPrice;
+      setTotalPrice(totalPrice - initialPrice)
     }
   return (
     <>
-    <View style={[styles.contact, styles.elvation]}>
+    <View style={styles.contact}>
+    < View style={[styles.secondC,  styles.elvation]}>
+    
      <ImageBackground 
-       source={require("./assets/BLACK.jpg")}
-       style={{height:'98%',width:'98.5%',margin:5}}>
+       source={background}
+       style={{height:'98.5%',width:'98.5%',marginTop:5, marginStart:5}}>
        <Text style={styles.name}>{name}</Text>
-      <Image style={styles.image} source={require('./assets/Egg.jpg')} />
+      <Image style={styles.image} source={profile} />
       <View style={styles.details}>
          
-         <Text style={{color: 'white'}}>Price: {price}</Text>
+         <Text style={{color: 'white'}}>Price: M{price}</Text>
          <Text style={{color: 'white'}}>Iterms: {iterms}</Text>
          <View style={styles.Buttons}>
          <View style={[styles.Button1, styles.elvation]}>
@@ -36,6 +39,8 @@ export default function Contact({name, totalPrice, setTotalPrice,price }) {
          </View>
       </View>
      </ImageBackground>
+     
+     </View>
     </View>
     </>
   )
@@ -43,13 +48,27 @@ export default function Contact({name, totalPrice, setTotalPrice,price }) {
 
 const styles = StyleSheet.create({
     contact: {
-        marginTop: 10,
-        width:'98.5%',
-        marginLeft:4,
+        flex:1,
         flexDirection: 'row',
-        borderColor: 'white',
+        borderColor: 'rgb(189, 189, 189)',
+        alignItems: 'center',
+        justifyContent: 'center',
        
       },
+      secondC:{
+        height: '90%',
+        width: '98%',
+        marginTop: 25,
+        backgroundColor: 'white',
+       
+      },
+     /* Each: {
+        
+        width: '98%',
+        marginTop: 1,
+       // backgroundColor: 'white',
+        
+      },*/
       details: {
         marginLeft: 10,
       },
@@ -66,7 +85,7 @@ const styles = StyleSheet.create({
         color:'white'
       },
       elvation:{
-        elevation:20,
+        elevation:10,
         shadowColor:'black',
       },
       Buttons:{
@@ -78,10 +97,10 @@ const styles = StyleSheet.create({
     
       },
       Button1:{
-        marginTop:5,
+        marginTop:0,
        },
        Button2:{
-         marginTop:5,
+         marginTop:0,
          marginLeft:126,
        },
 })
